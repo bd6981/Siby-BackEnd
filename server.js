ed3a4c8e3128973bae9644efb0d507aab3d92276
bd6981 <brittanyjdavis22@gmail.com>
2023-01-03 14:48:27 -0500

    added to server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.get(endpoint, callback)
 
 app.get('/', function(req, res) {
-  res.send('Hello World')
+    res.sendFile(__dirname + '/index.html')
 })
 
 app.listen(3000, function() {
