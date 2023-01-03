d3660185de0033f2362f85bb0a155c28f4607d4a
bd6981 <brittanyjdavis22@gmail.com>
2023-01-03 15:23:12 -0500

    imported index, db, user to server

--- src/server.js
+++ src/server.js
@@ -1,12 +1,20 @@
 // console.log('May Node be with you')
+
+import express from "express";
+import cors from "cors";
+import config from "./config/index.mjs";
+import db from "./config/db.mjs";
+import userRouter from "./api/user.mjs";
+
 const express = require('express');
 const app = express();
 
-app.get(endpoint, callback)
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html')
-})
+
+app.use(cors({ origin: true }));
+app.use(express.json());
+app.use("/api/user", userRouter);
+
 
 app.listen(3000, function() {
   console.log('listening on 3000')
