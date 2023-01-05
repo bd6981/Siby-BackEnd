3c90658705a661723417ba93e8c418a69b524905
bd6981 <brittanyjdavis22@gmail.com>
2023-01-05 18:58:41 -0500

    heroku push corrections in controller

--- src/userController.js
+++ src/userController.js
@@ -1,41 +1,28 @@
-// require the Express module
+
 const express = require("express");
-// instantiate a router -- this will hold all the logic
-// for the URLs + methods for this resource
+
 const router = express.Router();
-// import the bookmark model
+
 const User = require("./user");
 
-// Add routes to the router object
-
-// // Index: GET all the bookmarks
-// router.get('/', async (req, res, next) => {
-// 	try {
-// 		// 1. Get all of the bookmarks from the DB
-// 		const bookmarks = await Bookmark.find({});
-// 		// 2. Send them back to the client as JSON
-// 		res.json(bookmarks);
-// 	} catch (err) {
-// 		// if there's an error, pass it on!
-// 		next(err);
-// 	}
-// });
-// Show: Get a Bookmark by ID
-// router.get('/:id', (req, res, next) => {
-// 	console.log(req.params);
-// });
-// Show: Get a Bookmark by ID
-router.get("/:id", async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
-    // 1. Find the Bookmark by its unique ID
-    const user = await user.findById(req.params.id);
-    // 2. Send it back to the client as JSON
+    const user = await user.find({});
     res.json(user);
   } catch (err) {
-    // if there's an error, pass it on!
     next(err);
   }
 });
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const user = await user.findById(req.params.id);
+    res.json(user);
+  } catch (err) {
+    next(err);
+  }
+});
+
 // Create: POST a Bookmark
 router.post("/", async (req, res, next) => {
   try {
@@ -48,11 +35,10 @@ router.post("/", async (req, res, next) => {
     next(err);
   }
 });
+
+
 router.put("/:id", async (req, res, next) => {
   try {
-    // 1. Find the Bookmark by its id, passing in two additional arguments:
-    // the request body holds the updated information
-    // { new: true } returns the updated document instead of the old one
     const userToUpdate = await User.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
     });
@@ -71,9 +57,6 @@ router.put("/:id", async (req, res, next) => {
 // Delete: DELETE a Bookmark
 router.delete("/:id", async (req, res, next) => {
   try {
-    // 1. Find the Bookmark by its id, passing in two additional arguments:
-    // the request body holds the updated information
-    // { new: true } returns the updated document instead of the old one
     const userToDelete = await User.findByIdAndDelete(req.params.id);
     console.log(userToDelete);
     // If a bookmark was found and operation successful
